bd1da403b64930662dd75629937e9255cdd7b415
Esteban Repupilli <esteban.r@outlook.com>
2026-03-14 17:51:17 -0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update -y
